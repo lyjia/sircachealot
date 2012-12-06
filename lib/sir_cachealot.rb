1be21c70f10a77d1d741e972ec0d48c367d89b93
Tom Corelis <tom.corelis@gmail.com>
2012-12-06 03:40:12 -0800

    added crude deep copy support

--- lib/sir_cachealot.rb
+++ lib/sir_cachealot.rb
@@ -2,216 +2,229 @@ require "sir_cachealot/version"
 
 module Sir
 
-  RAM = :ram_cache
+	RAM = :ram_cache
 
-  @@ram_cache = { }
+	@@ram_cache = { }
 
-  @@configuration = {
-    mode:           RAM,
-    debug:          false,
-    annoy:          false, #super annoying debug messages
-    default_expiry: 3600, #Integer(1.hour),
-    delete_on_nil:  true
+	@@configuration = {
+		mode:           RAM,
+		debug:          false,
+		annoy:          false, #super annoying debug messages
+		default_expiry: 3600, #Integer(1.hour),
+		delete_on_nil:  true
 
-  }
+	}
 
-  #Configuration parameters
-  # configure() yields a block, as per convention
-  # configure(key) returns the value of key, or nil
-  # configure(key, value) sets key to value
-  def self.configure(key = nil, value = nil)
+	#Configuration parameters
+	# configure() yields a block, as per convention
+	# configure(key) returns the value of key, or nil
+	# configure(key, value) sets key to value
+	def self.configure(key = nil, value = nil)
 
-    key = key.downcase.to_sym unless key.nil?
+		key = key.downcase.to_sym unless key.nil?
 
-    if !key.nil? && !value.nil?
-      @@configuration[key] = value
-      return value
+		if !key.nil? && !value.nil?
+			@@configuration[key] = value
+			return value
 
-    elsif !key.nil? && value.nil?
-      return @@configuration[key]
+		elsif !key.nil? && value.nil?
+			return @@configuration[key]
 
-    elsif key.nil? && value.nil?
-      yield(@@configuration)
+		elsif key.nil? && value.nil?
+			yield(@@configuration)
 
-      #normalize keynames
-      @@configuration.keys.each do |k|
+			#normalize keynames
+			@@configuration.keys.each do |k|
 
-        knew = k.downcase.to_sym
+				knew = k.downcase.to_sym
 
-        unless knew == k
-          @@configuration[knew] = @@configuration[k]
-          @@configuration.delete(k)
-        end
+				unless knew == k
+					@@configuration[knew] = @@configuration[k]
+					@@configuration.delete(k)
+				end
 
-      end
+			end
 
-    end
+		end
 
-  end
-
-  class << self
-    alias :config :configure
-  end
+	end
 
 
-  # Gets value for supplied key
-  # Returns nil if empty or expired
-  def self.get(key)
-
-    case config(:mode)
-    when RAM
-
-      if x = @@ram_cache[key]
-
-        if x[:expiry].nil? || x[:expiry] > Time.now
-          return x[:value]
-        else
-          # cache entry is stale
-          puts ("          SirCachealot: Cache entry <#{key}> expired at #{x[:expiry]}. Deleting...") if config(:debug)
-          @@ram_cache.delete(key)
-          if block_given?
-            yield
-          else
-            nil
-          end
-        end
-
-      else
-
-        if block_given?
-          yield
-        else
-          return nil
-        end
-      end
-
-    else
-      puke
-    end
-
-  end
-  
-  # deletes the specified key from the cache.
-  # returns true if successful, false if not found
-  def self.delete(key)
-    
-    case config(:mode)
-    when RAM
-      if @@ram_cache.has_key?(key)
-        @@ram_cache.delete(key)
-        return true
-      end
-    end
-    
-  end
-
-  # Puts value in cache, key is param key
-  # Param expiry is optional, it can be a relative or absolute Fixnum or Time object
-  # Returns the value you put into it, unless config(:delete_on_nil) == true and value == nil.
-  # Returns true (because it deleted the key) if config(:delete_on_nil) == true and value == nil.
-  def self.put(key, value, expiry = config(:default_expiry))
-
-    case expiry.class.name
-    when "Fixnum"
-      true
-    when "Time"
-      expiry = Integer(expiry)
-    when "NilClass"
-      true
-    else
-      raise ArgumentError, "Expiry must be a Fixnum or Time object"
-    end
+	class << self
+		alias :config :configure
+	end
 
 
-    unless (!expiry.nil? && expiry > Integer(Time.now))
-      expiry = Time.now + expiry
-    end
+	# Gets value for supplied key
+	# Returns nil if empty or expired
+	def self.get(key, copy = false)
 
-    puts "          SirCachealot: Will expire <#{key}> at #{expiry}" if config(:annoy) == true
+		to_ret = nil
 
-    if config(:delete_on_nil) == true && value == nil
-        self.delete(key)
-    else
-      case config(:mode)
-      when RAM
+		case config(:mode)
+			when RAM
 
-        @@ram_cache[key]          ||= { }
-        @@ram_cache[key][:value]  = value
-        @@ram_cache[key][:expiry] = expiry
-        return value
+				if x = @@ram_cache[key]
 
-      else
-        puke
-      end      
-    end
-    
+					if x[:expiry].nil? || x[:expiry] > Time.now
+						to_ret = x[:value]
+					else
+						# cache entry is stale
+						puts ("          SirCachealot: Cache entry <#{key}> expired at #{x[:expiry]}. Deleting...") if config(:debug)
+						@@ram_cache.delete(key)
 
+					end
 
-  end
+				end
 
-  # Gets the qty of keys in the cache
-  def self.size?
+			else
+				puke
+		end
 
-    case config(:mode)
-    when RAM
-      return @@ram_cache.count
-    else
-      puke
-    end
+		if copy
+			to_ret = self.crude_clone(to_ret)
+		end
 
-  end
+		if to_ret.nil? && block_given?
+			to_ret = yield
+		end
 
-  # Dumps list of cache keys to console, with their value's class.name and expiry
-  def self.dump
+		return to_ret
 
-    case config(:mode)
-    when RAM
-      @@ram_cache.each do |k, v|
-        puts("%-20s %-20s %20s" % [k, v[:value].class, v[:expiry]])
-      end
-    else
-      puke
-    end
+	end
 
 
-    return nil
+	# deletes the specified key from the cache.
+	# returns true if successful, false if not found
+	def self.delete(key)
 
-  end
+		case config(:mode)
+			when RAM
+				if @@ram_cache.has_key?(key)
+					@@ram_cache.delete(key)
+					return true
+				end
+		end
 
-  # Clears the cache, erases all cache entries
-  def self.clear
+	end
 
-    case config(:mode)
-    when RAM
-      @@ram_cache = { }
-    else
-      puke
-    end
 
-  end
+	# Puts value in cache, key is param key
+	# Param expiry is optional, it can be a relative or absolute Fixnum or Time object
+	# Returns the value you put into it, unless config(:delete_on_nil) == true and value == nil.
+	# Returns true (because it deleted the key) if config(:delete_on_nil) == true and value == nil.
+	def self.put(key, value, expiry = config(:default_expiry))
 
-  # Sweeps the cache for expired keys and purges them. NOT THREAD-SAFE!
-  def self.clean
+		case expiry.class.name
+			when "Fixnum"
+				true
+			when "Time"
+				expiry = Integer(expiry)
+			when "NilClass"
+				true
+			else
+				raise ArgumentError, "Expiry must be a Fixnum or Time object"
+		end
 
-    case config(:mode)
 
-    when RAM
-      @@ram_cache.each_key do |k|
-        if !@@ram_cache[k][:expiry].nil? && @@ram_cache[k][:expiry] < Time.now
-          puts("          SirCachealot: Cleaned #{k}") if config(:debug)
-          @@ram_cache.delete(k)
-        end
-      end
-    else
-      puke
-    end
+		unless (!expiry.nil? && expiry > Integer(Time.now))
+			expiry = Time.now + expiry
+		end
 
-  end
+		puts "          SirCachealot: Will expire <#{key}> at #{expiry}" if config(:annoy) == true
 
-  private
+		if config(:delete_on_nil) == true && value == nil
+			self.delete(key)
+		else
+			case config(:mode)
+				when RAM
 
-  def self.puke
-    raise TypeError, "Invalid config(:mode). Check the inputs sent to Sir.configure()"
-  end
+					@@ram_cache[key]          ||= { }
+					@@ram_cache[key][:value]  = value
+					@@ram_cache[key][:expiry] = expiry
+					return value
+
+				else
+					puke
+			end
+		end
+
+
+	end
+
+
+	# Gets the qty of keys in the cache
+	def self.size?
+
+		case config(:mode)
+			when RAM
+				return @@ram_cache.count
+			else
+				puke
+		end
+
+	end
+
+
+	# Dumps list of cache keys to console, with their value's class.name and expiry
+	def self.dump
+
+		case config(:mode)
+			when RAM
+				@@ram_cache.each do |k, v|
+					puts("%-20s %-20s %20s" % [k, v[:value].class, v[:expiry]])
+				end
+			else
+				puke
+		end
+
+
+		return nil
+
+	end
+
+
+	# Clears the cache, erases all cache entries
+	def self.clear
+
+		case config(:mode)
+			when RAM
+				@@ram_cache = { }
+			else
+				puke
+		end
+
+	end
+
+
+	# Sweeps the cache for expired keys and purges them. NOT THREAD-SAFE!
+	def self.clean
+
+		case config(:mode)
+
+			when RAM
+				@@ram_cache.each_key do |k|
+					if !@@ram_cache[k][:expiry].nil? && @@ram_cache[k][:expiry] < Time.now
+						puts("          SirCachealot: Cleaned #{k}") if config(:debug)
+						@@ram_cache.delete(k)
+					end
+				end
+			else
+				puke
+		end
+
+	end
+
+
+	private
+
+	def self.puke
+		raise TypeError, "Invalid config(:mode). Check the inputs sent to Sir.configure()"
+	end
+
+
+	def self.crude_clone(obj)
+		return Marshal.load(Marshal.dump(obj))
+	end
 
 end
--- spec/sir_cachealot_spec.rb
+++ spec/sir_cachealot_spec.rb
@@ -1,84 +1,109 @@
 require 'spec_helper'
 
-TEST = "test"
+TEST           = "test"
 DEFAULT_EXPIRY = 600
 
 describe SirCachealot do
 
-  it 'should be configurable' do
+	it 'should be configurable' do
 
-    Sir.configure do |config|
-      config[:default_expiry] = DEFAULT_EXPIRY
-      config["MODE"]          = :ram_cache
-      config[:debug]          = true
-    end
+		Sir.configure do |config|
+			config[:default_expiry] = DEFAULT_EXPIRY
+			config["MODE"]          = :ram_cache
+			config[:debug]          = true
+		end
 
-    (Sir.config(:default_expiry) == DEFAULT_EXPIRY && Sir.config("MODE") == :ram_cache).should == true
+		(Sir.config(:default_expiry) == DEFAULT_EXPIRY && Sir.config("MODE") == :ram_cache).should == true
 
-  end
+	end
 
-  it 'should return the key that is put into it' do
-    Sir.put(:test, TEST).should == TEST
-  end
+	it 'should return the key that is put into it' do
+		Sir.put(:test, TEST).should == TEST
+	end
 
-  it 'should return an arbitrary key' do
+	it 'should return an arbitrary key' do
 
-    (Sir.get(:test) == TEST).should == true
+		(Sir.get(:test) == TEST).should == true
 
-  end
+	end
 
-  it 'should yield when given a block and a key that does not exist' do
-    (Sir.get(:asdoiajdoaijdaodijaodiajdoaidjaodijaodij) { 5 }).should == 5
-  end
+	it 'should yield when given a block and a key that does not exist' do
+		(Sir.get(:asdoiajdoaijdaodijaodiajdoaidjaodijaodij) { 5 }).should == 5
+	end
 
-  it 'should return nil when not given a block and a key that does not exist' do
-        (Sir.get(:asdoiajdoaijdaodijaodiajdoaidjaodijaodij)).should == nil
-  end
+	it 'should return nil when not given a block and a key that does not exist' do
+		(Sir.get(:asdoiajdoaijdaodijaodiajdoaidjaodijaodij)).should == nil
+	end
 
-  it 'should immediately expire a 0-expiry key' do
-    Sir.put(:expire, TEST, 0)
-    sleep(1)
+	it 'should immediately expire a 0-expiry key' do
+		Sir.put(:expire, TEST, 0)
+		sleep(1)
 
-    Sir.get(:expire).should == nil
-  end
+		Sir.get(:expire).should == nil
+	end
 
-  it 'should report its size correctly' do
+	it 'should report its size correctly' do
 
-    Sir.size?.should == 1
+		Sir.size?.should == 1
 
-  end
+	end
 
-  it 'should clean() correctly' do
+	it 'should clean() correctly' do
 
-    Sir.clear
+		Sir.clear
 
-    Sir.put(:clean, TEST, 0)
-    Sir.clean
+		Sir.put(:clean, TEST, 0)
+		Sir.clean
 
-    Sir.size?.should == 0
-    
-  end
+		Sir.size?.should == 0
 
-  it 'should dump() correctly' do
-    Sir.put(:dump, TEST)
-    Sir.dump
-  end
-  
-  it 'should accept a nil-expiry key' do
-    Sir.put(:test_nil, TEST).should == TEST
-  end
-  
-  it 'should not expire a nil-expiry key' do
-    Sir.get(:test_nil).should == TEST
-  end
-  
-  it 'should auto-delete a key when fed a nil' do
-    Sir.put(:test_nil, nil).should == true
-  end
-  
-  it 'should delete a key if specified' do
-    Sir.put(:delete_me, TEST)
-    Sir.delete(:delete_me).should == true
-  end
+	end
+
+	it 'should dump() correctly' do
+		Sir.put(:dump, TEST)
+		Sir.dump
+	end
+
+	it 'should accept a nil-expiry key' do
+		Sir.put(:test_nil, TEST).should == TEST
+	end
+
+	it 'should not expire a nil-expiry key' do
+		Sir.get(:test_nil).should == TEST
+	end
+
+	it 'should auto-delete a key when fed a nil' do
+		Sir.put(:test_nil, nil).should == true
+	end
+
+	it 'should delete a key if specified' do
+		Sir.put(:delete_me, TEST)
+		Sir.delete(:delete_me).should == true
+	end
+
+	it 'should return a shallow copy by default' do
+		hash = { :a => { :aa => 0 } }
+		Sir.put(:shallow_copy, hash)
+
+		newhash = Sir.get(:shallow_copy)
+
+		newhash[:a][:aa] = 1
+
+		(hash[:a][:aa]).should == newhash[:a][:aa]
+
+	end
+
+	it 'should return a deep copy when asked' do
+		hash = { :a => { :aa => "0" } }
+		Sir.put(:deep_copy, hash)
+
+		newhash = Sir.get(:deep_copy, true)
+
+		newhash[:a][:aa] = "1"
+
+		(hash[:a][:aa]).should_not == newhash[:a][:aa]
+
+	end
+
 
 end
